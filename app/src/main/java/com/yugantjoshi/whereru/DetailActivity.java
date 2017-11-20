ca58fe286ab6bf15fe43343fb28361ad44a96833
Yugant Joshi <joshi.yugant@gmail.com>
2017-11-20 16:12:48 -0500

    update UI with appropriate API data

--- app/src/main/java/com/yugantjoshi/whereru/DetailActivity.java
+++ app/src/main/java/com/yugantjoshi/whereru/DetailActivity.java
@@ -1,6 +1,7 @@
 package com.yugantjoshi.whereru;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
@@ -24,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class DetailActivity extends AppCompatActivity {
 
     TextView description, street, cityStatePostal;
-
+    String longitude, latitude;
     public static final String BASE_URL = "http://rumaps.rutgers.edu/bldgnum/";
     int buildingNum = -1;
     @Override
@@ -34,6 +35,8 @@ public class DetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        CollapsingToolbarLayout toolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+
         description = (TextView)findViewById(R.id.detail_description);
         street = (TextView)findViewById(R.id.street);
         cityStatePostal = (TextView)findViewById(R.id.city_state_postal);
@@ -41,8 +44,8 @@ public class DetailActivity extends AppCompatActivity {
         String title = getIntent().getStringExtra("buildingName");
         buildingNum = getIntent().getIntExtra("buildingNum",-1);
 
-        toolbar.setTitle(title);
         Log.d("buildingNum", String.valueOf(buildingNum));
+        toolbarLayout.setTitle(title);
 
         getCurrentBuildingInfoUpdate();
     }
@@ -59,7 +62,20 @@ public class DetailActivity extends AppCompatActivity {
         whereRUAPICall.enqueue(new Callback<WhereRUAPI>() {
             @Override
             public void onResponse(Call<WhereRUAPI> call, Response<WhereRUAPI> response) {
-                Log.d("RESPONSE",String.valueOf(response));
+
+                String descriptionOutput = String.valueOf(response.body().getDescription());
+                String streetOutput = String.valueOf( response.body().getLocation().getStreet());
+                String cityStatePostalOutput =String.valueOf(response.body().getLocation().getCity()
+                        +", "
+                        +response.body().getLocation().getState()+" "
+                        +response.body().getLocation().getPostalCode());
+
+                description.setText(descriptionOutput);
+                street.setText(streetOutput);
+                cityStatePostal.setText(cityStatePostalOutput);
+
+                longitude  = response.body().getLocation().getLongitude();
+                latitude = response.body().getLocation().getLatitude();
             }
 
             @Override
